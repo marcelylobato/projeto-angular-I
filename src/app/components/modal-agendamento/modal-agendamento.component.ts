e738178b55490d6d444b42ef4024e24a3b73e4a6
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-11-02 16:54:29 -0300

    Cria componente modal para informações de agendamento

---
+++ src/app/components/modal-agendamento/modal-agendamento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-agendamento',
+  templateUrl: './modal-agendamento.component.html',
+  styleUrl: './modal-agendamento.component.scss'
+})
+export class ModalAgendamentoComponent {
+
+}
