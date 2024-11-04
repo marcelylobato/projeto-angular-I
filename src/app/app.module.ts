8dc1499be7195ed2c9ddf96154c8aef7537ddc64
cezaniltom <cezaniltom@outlook.com>
2024-11-04 11:11:05 -0300

    importação de arquivo para o app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BtnInputsFormComponent } from './components/btn-inputs-form/btn-inputs-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BtnInputsFormComponent
   ],
   imports: [
     BrowserModule,
