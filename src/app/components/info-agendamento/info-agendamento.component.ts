6a51cce80dcbcac14e01ac553e9fb621c03e5c8d
Tata <nataliakuester@gmail.com>
2024-11-06 16:06:26 -0300

    Edita HTML e cria SCSS

--- src/app/components/info-agendamento/info-agendamento.component.ts
+++ src/app/components/info-agendamento/info-agendamento.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info-agendamento',
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class InfoAgendamentoComponent {
 
+
+
+
+agendamentos = [
+  { id: 1, nome: 'Julia', data: '11/11/2024', servico: 'Corte de Cabelo', horario: '14:00', valor: 70},
+  { id: 2, nome: 'Maria ', data: '11/11/2024', servico: 'Escova', horario: '15:00', valor: 80 },
+  { id: 3, nome: 'Ana', data: '12/11/2024', servico: 'Tintura', horario: '16:00', valor: 120 },
+]
 }
-
-
