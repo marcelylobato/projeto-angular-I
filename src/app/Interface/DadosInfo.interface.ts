8d10f7da522d342194f73cd065527511c8660895
Tata <nataliakuester@gmail.com>
2024-11-07 16:37:30 -0300

    Cria interface

---
+++ src/app/Interface/DadosInfo.interface.ts
@@ -0,0 +1,8 @@
+export interface DadosInfo {
+    id: number;
+    nome: string;
+    data: string;
+    servico: string;
+    horario: string;
+    valor: number;
+  }
--- src/app/components/info-agendamento/info-agendamento.component.ts
+++ src/app/components/info-agendamento/info-agendamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { DadosInfo } from '../../Interface/DadosInfo.interface';
 
 @Component({
   selector: 'app-info-agendamento',
@@ -8,11 +9,5 @@ import { Component, Input } from '@angular/core';
 export class InfoAgendamentoComponent {
 
 
-
-
-agendamentos = [
-  { id: 1, nome: 'Julia', data: '11/11/2024', servico: 'Corte de Cabelo', horario: '14:00', valor: 70},
-  { id: 2, nome: 'Maria ', data: '11/11/2024', servico: 'Escova', horario: '15:00', valor: 80 },
-  { id: 3, nome: 'Ana', data: '12/11/2024', servico: 'Tintura', horario: '16:00', valor: 120 },
-]
+  @Input() nomes: DadosInfo[] = []; 
 }
