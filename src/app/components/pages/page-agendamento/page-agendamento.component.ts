481b89809a2179d4be7c93055e8bcf7253d76c98
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-11-08 16:25:17 -0300

    Refatora estilos parcial

--- src/app/components/pages/page-agendamento/page-agendamento.component.ts
+++ src/app/components/pages/page-agendamento/page-agendamento.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './page-agendamento.component.scss'
 })
 export class PageAgendamentoComponent {
-
+  nomes = [
+    { id: 1, nome: 'Julia', data: '11/11/2024', servico: 'Corte de Cabelo', horario: '14:00', valor: 70},
+    { id: 2, nome: 'Maria ', data: '11/11/2024', servico: 'Escova', horario: '15:00', valor: 80 },
+    { id: 3, nome: 'Cleide', data: '12/11/2024', servico: 'Tintura', horario: '16:00', valor: 120 },
+  ]
 }
